b0175888a968f6df1b4bb47d599e6f1d762d2d28
Maxim Fomov <maksimfomov26@gmail.com>
2022-11-30 12:55:22 +0300

    feat: Added authorization

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBD8retLQAlp8dKbo1pEsTFsz7jej_ojL0",
+  authDomain: "tutor-order.firebaseapp.com",
+  projectId: "tutor-order",
+  storageBucket: "tutor-order.appspot.com",
+  messagingSenderId: "488506505715",
+  appId: "1:488506505715:web:b00e044f50f8d435337a86"
+};
+
+const app = initializeApp(firebaseConfig);
